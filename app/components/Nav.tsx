c12250777c897ef159b1391b9791cbb5ca07f79b
Zach Hite <zfhite@gmail.com>
2026-06-07 14:14:38 -0500

    Use pre-transparent windsock PNG with invert filter

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -31,11 +31,8 @@ export default function Nav() {
           className="flex items-center gap-0 shrink-0 justify-center"
           style={{ color: '#fdfcf8', width: 336, marginLeft: -24 }}
         >
-          <svg width="92" height="92" viewBox="100 230 600 580" fill="none" stroke="#fdfcf8" strokeWidth="28" strokeLinecap="round" strokeLinejoin="round">
-            <line x1="400" y1="270" x2="400" y2="760" />
-            <ellipse cx="400" cy="395" rx="52" ry="108" />
-            <path d="M400 287 Q560 310 750 390 Q760 420 750 450 Q560 480 400 503" />
-          </svg>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/windsock.png" alt="" width={92} height={92} style={{ filter: 'invert(1)', objectFit: 'contain' }} />
           <span
             className="font-semibold text-sm"
             style={{ letterSpacing: '0.22em', textTransform: 'uppercase' }}
